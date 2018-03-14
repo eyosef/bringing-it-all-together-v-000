3c9c3a8cc45b69bf74dda4574f6952b19297c995
Eden Mesfun <emesfun@gmail.com>
2018-03-14 17:46:06 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -38,7 +38,7 @@ class Dog
     SQL
 
     DB[:conn].execute(sql, self.name, self.breed)
-    @id = DB[:conn].execute("SELECT last_insert_rowid()")
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")
   end
 
 end
