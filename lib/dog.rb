9ab03015ee481068c60e10cc000b1d5aba159357
Eden Mesfun <emesfun@gmail.com>
2018-03-14 18:34:15 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -57,7 +57,7 @@ class Dog
   end
 
   def self.find_or_create_by(hash)
-    dog = DB[:connect].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?")
+    dog = DB[:connect].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", hash[:name], hash[:breed])
 
     # hash = {:name=>"teddy", :breed=>"cockapoo"}
   end
