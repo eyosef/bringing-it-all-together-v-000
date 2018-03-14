5161f7f6cc76e91460d6513b744e7861dbc3f70c
Eden Mesfun <emesfun@gmail.com>
2018-03-14 18:19:47 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -32,15 +32,14 @@ class Dog
 
   def save #FIX
       if self
-        self 
+        self
+        sql=<<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+        SQL
 
-      sql=<<-SQL
-      INSERT INTO dogs (name, breed)
-      VALUES (?, ?)
-      SQL
-
-      DB[:conn].execute(sql, self.name, self.breed)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+        DB[:conn].execute(sql, self.name, self.breed)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
 
   end #save
 
