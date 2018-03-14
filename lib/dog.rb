a211b82bfb47e0c98ba77dc4ed7b9d95ced3f2f7
Eden Mesfun <emesfun@gmail.com>
2018-03-14 18:51:52 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -68,7 +68,7 @@ class Dog
 
   def self.new_from_db(attribute)
     hash = {name: attribute[1], breed: attribute[2]}
-    new_dog = self.new(hash,  attribute[0])
+    new_dog = self.new(hash, attribute[0])
     new_dog
     # [1, "Pat", "poodle"]
   end
