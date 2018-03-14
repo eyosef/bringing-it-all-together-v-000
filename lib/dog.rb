e9624289a6e35e0a244c5117641535c34cd16764
Eden Mesfun <emesfun@gmail.com>
2018-03-14 19:24:01 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -76,7 +76,7 @@ class Dog
   end
 
   def self.find_by_name(name)
-    DB[:conn].execute("SELECT * FROM dogs WHERE name = ?", name)
+    dog DB[:conn].execute("SELECT * FROM dogs WHERE name = ?", name)
   end
 
 end #class
