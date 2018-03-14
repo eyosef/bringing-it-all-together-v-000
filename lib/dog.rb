76d3305e8b0261056b56d51ac9d82c8775f6665a
Eden Mesfun <emesfun@gmail.com>
2018-03-14 19:19:03 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -53,8 +53,8 @@ class Dog
   end
 
   def self.find_by_id(id)
-    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", hash[:name], hash[:breed])
-    
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE id = ?", hash[:name], hash[:breed])
+
     @@all.each { |instance| return instance if instance.id == id}
   end
 
