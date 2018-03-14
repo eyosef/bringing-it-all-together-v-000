21157b64748fd704f377d3b2039ba3171d695a11
Eden Mesfun <emesfun@gmail.com>
2018-03-14 18:30:09 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -57,7 +57,7 @@ class Dog
   end
 
   def self.find_or_create_by(name, breed)
-    hash = {}
+    hash = {name: name, breed: breed}
 
     @@all.each do |instance|
       if instance.name != name && instance.breed != breed
